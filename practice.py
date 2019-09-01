3711fd0dd53527c1670bae8b5eb84fcc7b1065c2
dryoungg222 <dr.youngg222@gmail.com>
2019-09-01 19:13:29 +0900

    add my_module [MUL]

--- practice.py
+++ practice.py
@@ -1,3 +1,8 @@
 Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> 
+>>> def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
